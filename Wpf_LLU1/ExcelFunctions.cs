ee583ad833871266983b5a9e989a8741a90079d4
Lars Lund <llu@alfapeople.com>
2019-02-10 12:45:18 +0100

    Add project files.

---
+++ Wpf_LLU1/ExcelFunctions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace Wpf_LLU1
+{
+    class ExcelFunctions
+    {
+        private static Excel.Workbook MyBook = null;
+        private static Excel.Application MyApp = null;
+        private static Excel.Worksheet MySheet = null;
+        private static Excel.Range MyRange = null;
+
+
+        public void CreateExcel(string DB_PATH)
+        {
+            MyApp = new Excel.Application();
+            MyApp.Visible = false;
+            MyBook = MyApp.Workbooks.Open(DB_PATH);
+            MySheet = (Excel.Worksheet)MyBook.Sheets[1]; // Explicit cast is not required here
+            int lastRow = MySheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
+            List<string> EmpList = new List<string> {};
+
+
+            for (int index = 2; index <= lastRow; index++)
+            {
+                System.Array MyValues = (System.Array)MySheet.get_Range("A" + index.ToString(), "F" + index.ToString()).Cells.Value;
+
+                
+                String Firstname = MyValues.GetValue(1, 4).ToString();
+                String LastName = MyValues.GetValue(1, 5).ToString(); 
+                EmpList.Add(Firstname+ " " + LastName);
+
+
+                MyRange = MySheet.Cells[index, 6].value(Firstname+" "+LastName);
+            }
+            MyBook.Save();
+        }
+    }
+}
---
+++ Wpf_LLU1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_LLU1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            /*StackPanel stackPanel = new StackPanel();
+            this.Content = stackPanel;
+
+            // Create the TextBlock 
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = "Welcome to XAML Tutorial";
+            textBlock.Height = 20;
+            textBlock.Width = 200;
+            textBlock.Margin = new Thickness(5);
+            stackPanel.Children.Add(textBlock);
+            
+            // Create the Button 
+            Button button = new Button();
+            button.Content = "OK";
+            button.Height = 20;
+            button.Width = 50;
+            button.Margin = new Thickness(20);
+            stackPanel.Children.Add(button);
+            */
+        }
+
+        private void btButton1_Click(object sender, RoutedEventArgs e)
+        {
+            ExcelFunctions ExcelFunctions = new ExcelFunctions();
+            ExcelFunctions.CreateExcel(@"c:\temp\mynames.xlsx");
+
+        }
+
+        private void btButton2_Click(object sender, RoutedEventArgs e)
+        {
+            WordFunctions wordFunctions = new WordFunctions();
+            wordFunctions.CreateWord();
+        }
+    }
+}
